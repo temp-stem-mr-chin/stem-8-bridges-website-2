ed25f22f9a4bc44f9f3e46f3aa60d053044794f1
temp-stem-mr-chin-student <github.garter086@passmail.net>
2024-12-09 11:56:26 -0800

    Updated teams

---
+++ teams.js
@@ -0,0 +1,20 @@
+
+var sites = [{
+    "name1": "Lucy",
+    "name2": "Keauna",
+    "folder": "lucy-keauna",
+    "index": "bridges.html"
+},{
+    "name1": "Lennox",
+    "name2": "Tayson",
+    "folder": "lennox-tayson"
+},{
+    "name1": "Akira",
+    "name2": "Tristan",
+    "folder": "akira-tristan"
+},{
+    "name1": "Elias",
+    "name2": "Max",
+    "folder": "Elias-Max"
+}
+];
